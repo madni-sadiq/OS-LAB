301215b9c75ee0a0c57d118e687c81ec548a9328
Madni <2017ee189@student.uet.edu.pk>
2021-04-25 02:33:51 +0500

    task 7.15 done

--- OS Lab 4/Fib7_15.c
+++ OS Lab 4/Fib7_15.c
@@ -12,37 +12,37 @@ To run
 #include <pthread.h>
 #include <stdlib.h>
 #include<unistd.h>
+
 int *fibser;
 int done = 0; 
 
 void *fibGen(void *Max_Points);/* the thread */
 
 void main(int argc, char *argv[]){
-int Max_Points;
-printf("Enter the number of fibonacci numbers you want: ");
-scanf("%d", &Max_Points);
-fibser= (int*)malloc(sizeof(int)*Max_Points);
- /* the thread identifier */
-pthread_attr_t attr; /* set of attributes for the thread */
-pthread_t tid[Max_Points];	
-/* get the default attributes */
-pthread_attr_init(&attr);
+	int Max_Points;
+	printf("Enter the number of fibonacci numbers you want: ");
+	scanf("%d", &Max_Points);
+	fibser= (int*)malloc(sizeof(int)*Max_Points);
+	 /* the thread identifier */
+	pthread_attr_t attr; /* set of attributes for the thread */
+	pthread_t tid[Max_Points];	
+	/* get the default attributes */
+	pthread_attr_init(&attr);
 
-for (int i=0; i<Max_Points; i++){
-pthread_create(&tid[i],&attr,fibGen, &Max_Points);
+	for (int i=0; i<Max_Points; i++){
+		pthread_create(&tid[i],&attr,fibGen, &Max_Points);
+	}
+	
+	while(1){
+		if (done){
+			for (int i=0; i<Max_Points; i++){
+			printf ("%d,", fibser[i]);
+			}
 
-
-}
-while(1){
-if (done){
-for (int i=0; i<Max_Points; i++){
-printf ("%d,", fibser[i]);
-}
-
-break;
-}
-}
-printf("\b\n");
+		break;
+		}
+	}
+	printf("\b\n");
 
 }
 
@@ -55,10 +55,10 @@ void *fibGen(void *Max_Points)
 	int n = *(int*) Max_Points;
 	
 	for (int i=0; i<2; i++){
-	fibser[i]=i;
+		fibser[i]=i;
 	}
 	for (int i=2; i<n; i++){
-	fibser[i] = fibser[i-1] + fibser[i-2];
+		fibser[i] = fibser[i-1] + fibser[i-2];
 	}
 	done = 1;
 	pthread_exit(0);
