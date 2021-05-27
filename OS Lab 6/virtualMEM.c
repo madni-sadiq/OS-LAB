1bc01f3ac028ebb44e9df5262f26b17149a285b7
Azhak Anwar <azhak.anwar@gmail.com>
2021-05-27 21:43:12 +0000

    Added TLB

--- OS Lab 6/virtualMEM.c
+++ OS Lab 6/virtualMEM.c
@@ -10,32 +10,50 @@
 #define PAGE_ENTRIES 256
 #define PAGE_SIZE 256
 #define MEM_SIZE 65536
+#define TLB_size 16
 
 
 int PageTable[PAGE_ENTRIES];
 signed char mem[MEM_SIZE];
 signed char *backing;
+
+struct TLB_unit{
+    char pageNo;
+    char frameNo;
+};
+
+typedef struct {
+    struct TLB_unit TLB_tbl[TLB_size];
+    unsigned int size = 0;
+} TLB_s;
+
 int main(int argc, char * argv[]) {
-	
-	
+
+
 	int addr;
-    	int pageno;
-    	int offset;
+    int pageno;
+    int offset;
 	int frameno;
-    	
+	int i;
+
+	TLB_s TLB;
+
 	FILE * fp; // file pointer to open addresses file
 	char input[INPUT_SIZE]; // input buffer
-	unsigned char page_demand = 0; // counter for counting page demands 
-	
+	unsigned char page_demand = 0; // counter for counting page demands
+
 	char *backing_file = "BACKING_STORE.bin"; // backing file for page demanding
     	int backing_fd = open(backing_file, O_RDONLY); // opening backing file with read only
-    	backing = mmap(0, MEM_SIZE, PROT_READ, MAP_PRIVATE, backing_fd, 0); // creating a mapping of backing file as an array 
+    	backing = mmap(0, MEM_SIZE, PROT_READ, MAP_PRIVATE, backing_fd, 0); // creating a mapping of backing file as an array
 
 
-	for (int i = 0; i < PAGE_ENTRIES ; i++){
+	for (i = 0; i < PAGE_ENTRIES ; i++){
 		// assigning every entry of page table a value of -1
 		PageTable[i] = -1;
-	} 
+	}
+	for(i = 0; i < TLB_size; i++){
+	    TLB.TLB_tbl[i].pageNo = -1;
+	}
 	if (argc == 2) {
         	fp = fopen(argv[1], "r"); // opening addresses file in read mode
         	if (fp < 0) {
@@ -43,29 +61,40 @@ int main(int argc, char * argv[]) {
             	return -1;
         	}
         }
-        
-        
-         while (fgets(input, INPUT_SIZE, fp) != NULL)
-       {	
+
+
+    while (fgets(input, INPUT_SIZE, fp) != NULL) {
        	addr = atoi(input); // getting logical address from file
-       	pageno = (addr & 65280) >> 8; // extracting page number from logical address i.e. bit 15 to bit 8
+       	pageno = (addr>>8) & 255; // extracting page number from logical address i.e. bit 15 to bit 8
     		offset = (addr & 255); // extracting offset from logical address i.e. bit 7 to bit 0
-    		if (PageTable[pageno] == -1){// when page is not found in page table 
-    			
+    		for(i = 0; i < TLB.size; i++){
+    		    if(TLB.TLB_tbl[i].pageNo == pageno){
+    		        frameno = TLB.TLB_tbl[i].frameNo;
+    		        break;
+    		    }
+    		}
+    		if (PageTable[pageno] == -1 && i == TLB.size){// when page is not found in page table
     			frameno = page_demand++;
-  
+
                 	// Copy page from backing file into physical memory
                 	memcpy(mem + frameno * PAGE_SIZE, backing + pageno * PAGE_SIZE, PAGE_SIZE);
                 	PageTable[pageno] = frameno; // saving page in page table
+                	TLB.TLB_tbl[TLB.size].pageNo = pageno;
+                	TLB.TLB_tbl[TLB.size++].frameNo = frameno;
+                	if(TLB.size == 16) TLB.size = 0;
+
     		}
-    		else{ // when page is availabe in page table 
+    		else if(i == TLB.size){ // when page is availabe in page table
     			frameno = PageTable[pageno];
+                TLB.TLB_tbl[TLB.size].pageNo = pageno;
+                TLB.TLB_tbl[TLB.size++].frameNo = frameno;
+                if(TLB.size == 16) TLB.size = 0;
     		}
-    	int physical_addr = (frameno * 256) + offset; // replacing frame number in place of page no for physical address 
-       signed char value = mem[physical_addr]; // getting value stored at physical aaddress
+    	int physical_addr = (frameno * 256) + offset; // replacing frame number in place of page no for physical address
+        signed char value = mem[physical_addr]; // getting value stored at physical aaddress
 	// printing addresses and value
        printf("Virtual address: %d Physical address: %d Value: %d\n", addr, physical_addr, value);
        }
-        
+
     	return -1;
  }
